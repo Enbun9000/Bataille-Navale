eea6590e10c32115aef22cd21740ca0755a7968b
MUMINOVIC Benjamin <Benjamin.MUMINOVIC@cpnv.ch>
2020-03-06 11:41:01 +0100

    Réalisation Menu

--- main.c
+++ main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
+void space(int nbr){
+    for(int i = 0; i<nbr; i++)
+    printf(" ");
+}
+
+void menu() {
+    printf("\n\n\n                                 === Bataille navale ===\n\n\n");
+
+    space(30);
+    printf("1. Jouer\n");
+    space(30);
+    printf("2. Login\n");
+    space(30);
+    printf("3. Scores\n");
+    space(30);
+    printf("4. Aide");
+    space(30);
+    printf("5. Quitter\n\n");
+}
 
 int main() {
-    printf("Hello, World!\n");
+
+    menu();
+
+    system("Pause");
     return 0;
+
 }
